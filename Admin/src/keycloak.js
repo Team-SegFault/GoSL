b1f9d9295b22e17f4bbdaff7b977afb345eedd4b
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-09-05 01:57:23 +0530

    added keycloak instructions

--- Admin/src/keycloak.js
+++ Admin/src/keycloak.js
@@ -1,9 +1,11 @@
 import Keycloak from "keycloak-js";
 
-const keycloak = new Keycloak({
-  url: "http://localhost:8080/auth", // Keycloak server URL
+const config = {
+  url: "http://localhost:8080/", // Keycloak server URL
   realm: "gosl", // Your realm name
   clientId: "gosl-admin", // Your client ID
-});
+};
+
+const keycloak = new Keycloak(config);
 
 export default keycloak;
---
+++ Admin/src/pages/DashboardPage.jsx
@@ -0,0 +1,10 @@
+const DashboardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>This is the dashboard page</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
