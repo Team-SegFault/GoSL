a410ef3829aed8ab37a02e5f99ecfef6dc489be6
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-09-05 10:21:16 +0530

    updated routes

--- Admin/src/pages/DashboardPage.jsx
+++ Admin/src/pages/DashboardPage.jsx
@@ -2,7 +2,16 @@ const DashboardPage = () => {
   return (
     <div>
       <h1>Dashboard</h1>
-      <p>This is the dashboard page</p>
+      <p>
+        This is the dashboard page will have the list of pending visa
+        applications, organized by priority or submission date.
+      </p>
+      <p>
+        Filters (e.g., by status: "pending", "under review", "approved",
+        "denied") and sorting options (e.g., by submission date, applicant
+        name). Key information such as the applicant's name, application date,
+        country of origin, and current status.
+      </p>
     </div>
   );
 };
