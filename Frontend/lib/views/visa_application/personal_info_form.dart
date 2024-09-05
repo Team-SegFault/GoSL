3420f2503f9ccb34403e536f0ef596e8cdbb5327
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-09-06 00:35:48 +0530

    updated form elements rendering to map

--- Frontend/lib/views/visa_application/personal_info_form.dart
+++ Frontend/lib/views/visa_application/personal_info_form.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:gap/gap.dart';
@@ -9,73 +11,86 @@ class PersonalInfoForm extends StatelessWidget {
 
   static const gap = Gap(20);
 
+  static List<Widget> elements  = [
+    FormBuilderTextField(
+      name: 'name',
+      decoration: const InputDecoration(labelText: 'Name'),
+    ),
+    FormBuilderDropdown(
+      name: 'gender',
+      decoration: const InputDecoration(labelText: 'Gender'),
+      items: ['Male', 'Female', 'Other']
+          .map(
+            (gender) =>
+            DropdownMenuItem(
+              value: gender,
+              child: Text(gender),
+            ),
+      )
+          .toList(),
+    ),
+    FormBuilderDateTimePicker(
+      name: 'birthdate',
+      decoration: const InputDecoration(labelText: 'Birthdate'),
+      inputType: InputType.date,
+      initialDate: DateTime.now(),
+      initialValue: DateTime.now(),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    ),
+    FormBuilderDropdown(
+      name: 'occupation',
+      decoration: const InputDecoration(labelText: 'Occupation'),
+      items: [
+        'Employed',
+        'Self-Employed',
+        'Student',
+        'Retired',
+        'Unemployed',
+        'Other',
+        'N/A'
+      ]
+          .map(
+            (gender) =>
+            DropdownMenuItem(
+              value: gender,
+              child: Text(gender),
+            ),
+      )
+          .toList(),
+    ),
+    FormBuilderDropdown(
+      name: 'gender',
+      decoration: const InputDecoration(labelText: 'Civil Status'),
+      items: ['Married', 'Single', 'Divorced', 'Widowed']
+          .map(
+            (gender) =>
+            DropdownMenuItem(
+              value: gender,
+              child: Text(gender),
+            ),
+      )
+          .toList(),
+    ),
+    SizedBox(
+      width: double.infinity,
+      child: AppButton(
+        text: 'Next',
+        onPressed: () {
+
+        },
+      ),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return FormBuilder(
       key: _formKey,
       child: Column(
+        // children - elements with gap in between
         children: [
-          FormBuilderTextField(
-            name: 'name',
-            decoration: const InputDecoration(labelText: 'Name'),
-          ),
-          gap,
-          FormBuilderDropdown(
-            name: 'gender',
-            decoration: const InputDecoration(labelText: 'Gender'),
-            items: ['Male', 'Female', 'Other']
-                .map(
-                  (gender) => DropdownMenuItem(
-                    value: gender,
-                    child: Text(gender),
-                  ),
-                )
-                .toList(),
-          ),
-          gap,
-          FormBuilderDateTimePicker(
-            name: 'birthdate',
-            decoration: const InputDecoration(labelText: 'Birthdate'),
-            inputType: InputType.date,
-            initialDate: DateTime.now(),
-            initialValue: DateTime.now(),
-            firstDate: DateTime(1900),
-            lastDate: DateTime.now(),
-          ),
-          gap,
-          FormBuilderDropdown(
-            name: 'occupation',
-            decoration: const InputDecoration(labelText: 'Occupation'),
-            items: ['Employed', 'Self-Employed', 'Student', 'Retired', 'Unemployed', 'Other', 'N/A']
-                .map(
-                  (gender) => DropdownMenuItem(
-                value: gender,
-                child: Text(gender),
-              ),
-            )
-                .toList(),
-          ),
-          gap,
-          FormBuilderDropdown(
-            name: 'gender',
-            decoration: const InputDecoration(labelText: 'Civil Status'),
-            items: ['Married', 'Single', 'Divorced', 'Widowed']
-                .map(
-                  (gender) => DropdownMenuItem(
-                value: gender,
-                child: Text(gender),
-              ),
-            )
-                .toList(),
-          ),
-          gap,
-          SizedBox(
-            width: double.infinity,
-            child: AppButton(
-              text: 'Next',
-              onPressed: () {},
-            ),
-          ),
+          for (var element in elements) ...[element, gap],
         ],
       ),
     );
@@ -100,7 +115,7 @@ class AppButton extends StatelessWidget {
           padding: const EdgeInsets.all(14),
           shape: RoundedRectangleBorder(
             borderRadius:
-                BorderRadius.circular(10), // Adjust the border radius as needed
+            BorderRadius.circular(10), // Adjust the border radius as needed
           ),
           elevation: 0),
       onPressed: onPressed,
