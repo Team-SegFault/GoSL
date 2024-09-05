da0d5790f3bfe509e20082c9094cf0a03a867886
Pavan Pitiwaduge <rajivapitiwaduge@gmail.com>
2024-09-05 11:07:22 +0530

    updated table

--- Admin/src/pages/DashboardPage.jsx
+++ Admin/src/pages/DashboardPage.jsx
@@ -1,9 +1,8 @@
-import Dashboard from "@/components/dashboard/pending";
+import Dashboard from "@/components/dashboard/applications";
 
 const DashboardPage = () => {
   return (
-    <div className="px-4 my-3 flex flex-col gap-2">
-      <h1 className="text-xl font-semibold">Dashboard</h1>
+    <div className="p-5">
       <Dashboard />
     </div>
   );
