deadb4d50f40cbde2b6667113b1d226872f9b28b
Ravindu Weerakoon <ravinduhiran26@gmail.com>
2024-09-05 23:28:47 +0530

    email functionality

---
+++ Backend/GoSLBackend/api/utils.py
@@ -0,0 +1,12 @@
+from django.core.mail import send_mail
+
+def send_mail(subject, message, email):
+    # Set the email details
+    from_email = 'ravinduhiran26@gmail.com'
+    recipient_list = [email]
+    
+    # Compose the email message
+    email_message = f"Congradulations! Your visa application has been approved."
+    
+    # Send the email
+    send_mail(subject, email_message, from_email, recipient_list)
