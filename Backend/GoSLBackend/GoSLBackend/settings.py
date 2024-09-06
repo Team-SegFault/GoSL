97c56cd4dff3b2c9de2d43b204db0e8b2bd5c4b6
yasanthaniroshan <niroshanyi@gmail.com>
2024-09-06 15:16:08 +0530

    Updated Send Mail Function (Not Working) - Updated API key to set through .env

--- Backend/GoSLBackend/GoSLBackend/settings.py
+++ Backend/GoSLBackend/GoSLBackend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -56,7 +60,9 @@ ROOT_URLCONF = 'GoSLBackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -128,5 +134,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 EMAIL_BACKEND = "sendgrid_backend.SendgridBackend"
-SENDGRID_API_KEY = "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"
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 SENDGRID_SANDBOX_MODE_IN_DEBUG=False
--- Backend/GoSLBackend/api/utils.py
+++ Backend/GoSLBackend/api/utils.py
@@ -1,13 +1,26 @@
 from django.core.mail import send_mail
+from datetime import datetime
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
 
-def send_mail(email):
-    # Set the email details
-    subject = 'Visa Application Approved'
-    from_email = 'ravinduhiran26@gmail.com'
-    recipient_list = [email]
-    
-    # Compose the email message
-    email_message = f"Congradulations! Your visa application has been approved."
-    
-    # Send the email
-    send_mail(subject, email_message, from_email, recipient_list)
+
+def confirm_visa_application(request):
+    recipient_name = "Lakindu Kariyawasam"
+    visa_type = "Tourist Visa"
+    approval_date = datetime.now()
+    expiration_date = approval_date
+    travel_dates = datetime.now()
+    your_full_name = "Ravindu Hiran"
+    context = {
+    "recipient_name":"Lakindu Kariyawasam",
+    "visa_type":"Tourist Visa",
+    "approval_date":datetime.now(),
+    "expiration_date":approval_date,
+    "travel_dates":datetime.now(),
+    "your_full_name":"Ravindu Hiran"}
+    convert_to_html_content =  render_to_string(template_name='email_templates.html',context=context)
+    plain_message = strip_tags(convert_to_html_content)
+    send_mail("Visa Application Confirmation",plain_message,"ravinduhiran26@gmail.com",["lakindu.21@cse.mrt.ac.lk"])
+    return "Email sent successfully!"
